ec7e206c6a6575e99cf4c63bc4c07b5a09cb87f5
zeshanbaig786 <zeshanbaig786@gmail.com>
2022-12-06 09:54:54 +0500

    task 2 completed glove test passed

--- src/main/java/Glove.java
+++ src/main/java/Glove.java
@@ -4,23 +4,27 @@ public class Glove {
 
     public Glove(String _vocabulary, Vector _vector) {
         //TODO Task 2.1
+        strVocabulary = _vocabulary;
+        vecVector = _vector;
     }
 
     public String getVocabulary() {
         //TODO Task 2.2
-        return null;
+        return strVocabulary;
     }
 
     public Vector getVector() {
         //TODO Task 2.3
-        return null;
+        return vecVector;
     }
 
     public void setVocabulary(String _vocabulary) {
         //TODO Task 2.4
+        strVocabulary = _vocabulary;
     }
 
     public void setVector(Vector _vector) {
         //TODO Task 2.5
+        vecVector = _vector;
     }
 }
