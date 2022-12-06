789eeb7b4c2e88a02bdce7098dcd0e990b53dd58
zeshanbaig786 <zeshanbaig786@gmail.com>
2022-12-06 09:59:54 +0500

    task 3 completed CosSimilarityPairTest passed

--- src/main/java/CosSimilarityPair.java
+++ src/main/java/CosSimilarityPair.java
@@ -5,47 +5,57 @@ public class CosSimilarityPair {
     private Vector vecVector;
 
     public CosSimilarityPair(String _word1, String _word2, double _cosinesimilarity) {
-        //TODO Task 3.1
+        //TODO_Completed Task 3.1
+        strWord1 = _word1;
+        strWord2 = _word2;
+        doubCS = _cosinesimilarity;
     }
 
     public CosSimilarityPair(Vector _vector, String _word2, double _cosinesimilarity) {
-        //TODO Task 3.2
+        //TODO_Completed Task 3.2
+        vecVector = _vector;
+        strWord2 = _word2;
+        doubCS = _cosinesimilarity;
     }
 
     public String getWord1() {
-        //TODO Task 3.3
-        return null;
+        //TODO_Completed Task 3.3
+        return strWord1;
     }
 
     public String getWord2() {
-        //TODO Task 3.4
-        return null;
+        //TODO_Completed Task 3.4
+        return strWord2;
     }
 
     public double getCosineSimilarity() {
-        //TODO Task 3.5
-        return 0;
+        //TODO_Completed Task 3.5
+        return doubCS;
     }
 
     public void setCosineSimilarity(double _cs) {
-        //TODO Task 3.6
+        //TODO_Completed Task 3.6
+        doubCS = _cs;
     }
 
     public void setWord1(String _word) {
-        //TODO Task 3.7
+        //TODO_Completed Task 3.7
+        strWord1 = _word;
     }
 
     public void setWord2(String _word) {
-        //TODO Task 3.8
+        //TODO_Completed Task 3.8
+        strWord2 = _word;
     }
 
     public Vector getVector() {
-        //TODO Task 3.9
-        return null;
+        //TODO_Completed Task 3.9
+        return vecVector;
     }
 
     public void setVector(Vector _vector) {
-        //TODO Task 3.10
+        //TODO_Completed Task 3.10
+        vecVector = _vector;
     }
 
 }
--- src/main/java/Glove.java
+++ src/main/java/Glove.java
@@ -3,28 +3,28 @@ public class Glove {
     private Vector vecVector;
 
     public Glove(String _vocabulary, Vector _vector) {
-        //TODO Task 2.1
+        //TODO_Completed Task 2.1
         strVocabulary = _vocabulary;
         vecVector = _vector;
     }
 
     public String getVocabulary() {
-        //TODO Task 2.2
+        //TODO_Completed Task 2.2
         return strVocabulary;
     }
 
     public Vector getVector() {
-        //TODO Task 2.3
+        //TODO_Completed Task 2.3
         return vecVector;
     }
 
     public void setVocabulary(String _vocabulary) {
-        //TODO Task 2.4
+        //TODO_Completed Task 2.4
         strVocabulary = _vocabulary;
     }
 
     public void setVector(Vector _vector) {
-        //TODO Task 2.5
+        //TODO_Completed Task 2.5
         vecVector = _vector;
     }
 }
